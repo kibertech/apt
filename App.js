17f9d6ec6dd0e78f7e8697cd1155218bf300f24f
kibertech <lukanindan@gmail.com>
2021-12-11 12:20:44 +0500

    добавил батон

--- App.js
+++ App.js
@@ -1,11 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert} from 'react-native';
 
 export default function App() {
+  function onPressLearnMore() {
+    Alert.alert('Simple Button pressed')
+  }
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      
+      <Button
+        onPress={onPressLearnMore}
+        title="Learn More"
+        color="#841584"
+        accessibilityLabel="Learn more about this purple button"
+      />
+
       <StatusBar style="auto" />
     </View>
   ); 
